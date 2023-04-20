00b26fe937546c51709c442f11b75afa200d258f
AmanRaj <89194819+AmanRaj073@users.noreply.github.com>
2023-04-20 18:27:40 +0530

    Pc

---
+++ script.js
@@ -0,0 +1,19 @@
+let outputscreen = document.getElementById("output-screen");
+function display(num) {
+    outputscreen.value += num;
+};
+
+function Calculate() {
+    try {
+        outputscreen.value = eval(outputscreen.value)
+    } catch (error) {
+        alert("Invalid")
+    }
+}
+
+function Clear() {
+    outputscreen.value = "";
+}
+function del() {
+    outputscreen.value = outputscreen.value.slice(0,-1)
+}
